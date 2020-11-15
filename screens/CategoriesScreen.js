dc322a71027beb32bc537ec4be7313fb807d2c0f
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-16 00:16:33 +0300

    inroduced nav method replace that replaces the current screen in the stack such that there's no back

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -9,8 +9,12 @@ const CategoriesScreen = props => {
       <Text>The Categories Screen</Text>
       <Button title="Category Meals Screen" onPress={() => {
         // props.navigation.navigate({ routeName: 'CategoryMeals' })
+
         props.navigation.navigate('CategoryMeals')
+
         // props.navigation.push('CategoryMeals')// used when navigating to the same scareen
+
+        // props.navigation.replace('CategoryMeals')//replaces the current screen in the stack and therefore there's no back
       }} />
     </View>
   )
