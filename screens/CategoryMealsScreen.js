96307a4991171f2081581770f3d14af771f3f762
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-16 00:02:20 +0300

    experimented with a number of methods used for navigation i.e going back one step, going back to the root screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
 
 const CategoryMealsScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen</Text>
+      <View style={styles.navButtonsContainer}>
+        <Button title="Meal Detail Screen" onPress={() => {
+          props.navigation.navigate('MealDetail')
+        }} />
+        <Button title="Go Back" onPress={() => {
+          // props.navigation.goBack()
+          props.navigation.pop()
+        }} />
+      </View>
     </View>
   )
 }
@@ -16,6 +25,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  navButtonsContainer: {
+    marginVertical: 5
   }
 })
 
