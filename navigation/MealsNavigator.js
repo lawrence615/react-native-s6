8ed7715c9b4f2f0c323cbd295f6a93a25dfc1182
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-15 04:07:33 +0300

    initial commit - defines files in the stack navigation

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,18 @@
+import { exp } from 'react-native/Libraries/Animated/src/Easing'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+
+import CategoriesScreen from '../screens/CategoriesScreen'
+import CategoryMealsScreen from '../screens/CategoryMealsScreen'
+import MealDetailScreen from '../screens/MealDetailScreen'
+
+
+const MealsNavigator = createStackNavigator({
+  Categories: CategoriesScreen,
+  CategoryMeals: {
+    screen: CategoryMealsScreen
+  },
+  MealDetail: MealDetailScreen
+})
+
+export default createAppContainer(MealsNavigator)
