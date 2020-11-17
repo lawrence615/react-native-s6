b29f8cec339f7befd0fefd2a85ea7c827a70014c
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-17 04:16:50 +0300

    moved repeated navigation options to defaultNavigationOptions param

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native'
 import { exp } from 'react-native/Libraries/Animated/src/Easing'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
@@ -6,13 +7,27 @@ import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
 
+import Colours from '../constants/Colours'
+
 
 const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
+  Categories: {
+    screen: CategoriesScreen
+  },
   CategoryMeals: {
-    screen: CategoryMealsScreen
+    screen: CategoryMealsScreen,
   },
   MealDetail: MealDetailScreen
+}, {
+  // mode:'modal', // shows on ios
+  // initialRouteName:'MealDetail',
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colours.primaryColour : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colours.primaryColour,
+    headerTitle: 'A Screen'
+  }
 })
 
 export default createAppContainer(MealsNavigator)
