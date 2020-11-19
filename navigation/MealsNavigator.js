cca29b51d5e71a2c1799685a25d0a623cf04b8f6
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-19 23:29:28 +0300

    set icons on the tabs and added some configurations using navigationOptions

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,8 +1,10 @@
+import React from 'react'
 import { Platform } from 'react-native'
 import { exp } from 'react-native/Libraries/Animated/src/Easing'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
@@ -33,8 +35,27 @@ const MealsNavigator = createStackNavigator({
 })
 
 const MealsFavTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator,
-  Favourites: FavouritesScreen
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
+      }
+    }
+  },
+  Favourites: {
+    screen: FavouritesScreen,
+    navigationOptions: {
+      tabBarLabel:'Favourites!', 
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+      }
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: Colours.accentColour
+  }
 })
 
 export default createAppContainer(MealsFavTabNavigator)
