5a761026e1dcdc4a102e2648b152a24ca15b7874
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-19 05:07:36 +0300

    replaced the view with a FlatList and an external custom component in rendering the view for the page

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,28 +1,40 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 
-import { CATEGORIES } from '../data/dummy-data'
+import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 
 const CategoryMealsScreen = props => {
 
   const catId = props.navigation.getParam('categoryId')
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  )
+
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {
+          props.navigation.navigate('MealDetail', { mealId: itemData.item.id })
+        }}
+      />
+    )
+  }
 
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <View style={styles.navButtonsContainer}>
-        <Button title="Meal Detail Screen" onPress={() => {
-          props.navigation.navigate('MealDetail')
-        }} />
-        <Button title="Go Back" onPress={() => {
-          // props.navigation.goBack()
-          props.navigation.pop()
-        }} />
-      </View>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={styles.mealsList} />
     </View>
   )
 }
@@ -40,7 +52,11 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 5
+  },
+  mealsList: {
+    width: '100%'
   },
   navButtonsContainer: {
     marginVertical: 5
