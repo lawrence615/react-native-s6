b83c98bb561ec0dcde3a6c5e113546cc51cb98dd
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-20 01:56:15 +0300

    The MealItem component is now being called from the MealList component

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
 
 import { CATEGORIES, MEALS } from '../data/dummy-data'
-import MealItem from '../components/MealItem'
+import MealList from '../components/MealList'
 
 
 const CategoryMealsScreen = props => {
@@ -13,30 +12,7 @@ const CategoryMealsScreen = props => {
     meal => meal.categoryIds.indexOf(catId) >= 0
   )
 
-  const renderMealItem = itemData => {
-    return (
-      <MealItem
-        title={itemData.item.title}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        image={itemData.item.imageUrl}
-        onSelectMeal={() => {
-          props.navigation.navigate('MealDetail', { mealId: itemData.item.id })
-        }}
-      />
-    )
-  }
-
-  return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item, index) => item.id}
-        renderItem={renderMealItem}
-        style={styles.mealsList} />
-    </View>
-  )
+  return <MealList listData={displayedMeals} navigation={props.navigation} />
 }
 
 CategoryMealsScreen.navigationOptions = (navigationData) => {
@@ -47,21 +23,6 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title
   }
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 5
-  },
-  mealsList: {
-    width: '100%'
-  },
-  navButtonsContainer: {
-    marginVertical: 5
-  }
-})
 
 
 export default CategoryMealsScreen
