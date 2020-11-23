76da7514c76bbf5be71f65a66d1459d47fb0000d
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-23 07:09:45 +0300

    Added dummy text to be shown when there are no favourite meals

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { View, StyleSheet } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { useSelector } from 'react-redux'
 
 import HeaderButton from '../components/HeaderButton'
 import MealList from '../components/MealList'
+import DefaultText from '../components/DefaultText'
 
 
 const FavouritesScreen = props => {
 
   const favMeals = useSelector(state => state.meals.favouriteMeals)
 
-  // const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2')
+  if (favMeals.length === 0 || !favMeals) {
+    return <View style={styles.empty}>
+      <DefaultText>No favourite meals found. Start adding some!!!</DefaultText>
+    </View>
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />
 }
@@ -25,5 +31,13 @@ FavouritesScreen.navigationOptions = navData => {
     </HeaderButtons>
   }
 }
+
+const styles = StyleSheet.create({
+  empty: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default FavouritesScreen
